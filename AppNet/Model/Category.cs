073c24ce50806275de8db22efe06c74fd345a220
Felipe <felipemiillerr@gmail.com>
2023-08-02 16:51:20 -0300

    add: model category

---
+++ AppNet/Model/Category.cs
@@ -0,0 +1,16 @@
+namespace AppNet.Model;
+
+public class Category
+{
+    public Guid Id { set; get; }
+    public string Nome { set; get; }
+    public DateTime CreationDate { get; set; }
+
+
+    public Category(string nome)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        CreationDate = DateTime.Now;
+    }
+}
